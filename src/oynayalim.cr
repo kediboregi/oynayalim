2fa1402f5b51a98ef9a9fd9fce34e64a9421172b
Oğuzhan Çankaya <unequaled86@gmail.com>
2019-03-18 17:56:53 +0300

    .

--- src/oynayalim.cr
+++ src/oynayalim.cr
@@ -1,6 +1,7 @@
-# TODO: Write documentation for `Oynayalim`
-module Oynayalim
-  VERSION = "0.1.0"
+require "kemal"
 
-  # TODO: Put your code here
+get "/" do
+  "Hello World!"
 end
+
+Kemal.run(ENV["PORT"])
