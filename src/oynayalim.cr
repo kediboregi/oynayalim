d28beb5fb5a35d7c733f7b17ab8ec62c7315c138
Oğuzhan Çankaya <unequaled86@gmail.com>
2019-03-23 15:23:11 +0300

    d

--- src/oynayalim.cr
+++ src/oynayalim.cr
@@ -1,7 +1,12 @@
 require "kemal"
+require "json"
 
 get "/" do
-  "Hello World!"
+	uyeler = [{ad: "ss"}, {ad: "dd"}]
+	hashd = Hash(String, JSON::Any).new
+	hashd["cCc"] = "hata"
+	hashd["uyeler"] = uyeler
+	hashd.to_json
 end
 
 Kemal.run(ENV["PORT"].to_i32.not_nil!)
