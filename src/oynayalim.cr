e4e5433a4530113b5f8889a433423bfadcfb7900
Oğuzhan Çankaya <unequaled86@gmail.com>
2019-03-23 15:50:21 +0300

    d

--- src/oynayalim.cr
+++ src/oynayalim.cr
@@ -1,11 +1,42 @@
 require "kemal"
 require "json"
 
+class ResponseWrapper(T)
+      JSON.mapping(code: Int32?,
+        status: String?,
+        data: Container(T)?)
+end
+
+class Container(T)
+      JSON.mapping(
+        offset: Int32?,
+        limit: Int32?,
+        total: Int32?,
+        count: Int32?,
+        results: Array(T)
+      )
+end
+
+class Character
+      JSON.mapping(id: Int32?, name: String?)
+end
+
+class Comic
+      JSON.mapping(id: Int32?, title: String?)
+end
+
+class MissingParameter
+  JSON.mapping(code: String, message: String)
+end
+
+alias ComicRequest = ResponseWrapper(Comic) | MissingParameter
+alias CharacterRequest = ResponseWrapper(CharacterRequest) | MissingParameter
+
 get "/" do
 	uyeler = [{"ad" => "ss"}, {"ad" => "dd"}]
 	hashd = {} of String => JSON::Any::Type
 	hashd["cCc"] = "hata"
-	hashd["uyeler"] = uyeler
+	hashd["uyeler"] = {"ad" => "ss"}
 	hashd.to_json
 end
 
