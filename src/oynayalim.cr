417e3efc75f8e9a1fee06e0765b440c73240617f
Oğuzhan Çankaya <unequaled86@gmail.com>
2019-03-23 15:28:00 +0300

    d

--- src/oynayalim.cr
+++ src/oynayalim.cr
@@ -3,7 +3,7 @@ require "json"
 
 get "/" do
 	uyeler = [{ad: "ss"}, {ad: "dd"}]
-	hashd = Hash(String, JSON::Any)
+	hashd = {} of String => JSON::Any
 	hashd["cCc"] = "hata"
 	hashd["uyeler"] = uyeler
 	hashd.to_json
