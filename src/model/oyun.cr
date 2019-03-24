a6c756bc2b737bd98bbc73dd5f56bf27489dc5be
Oğuzhan Çankaya <unequaled86@gmail.com>
2019-03-24 15:17:16 +0300

    d

--- src/model/oyun.cr
+++ src/model/oyun.cr
@@ -12,9 +12,9 @@ class ApiRes
 
 	def eksik()
 		if @eksikler.nil?
-			return False
+			true
 		else
-			return True
+			false
 		end
 	end
 
@@ -41,7 +41,7 @@ class OyunApiRes < ApiRes
 	def parse
 		res = {"ad" => @ad, "bitti" => @bitti}
 		if eksik
-			res.merge!({"eksikler" => eksikler})
+			res.merge!({"eksikler" => @eksikler})
 		end
 		res.to_json
 	end
