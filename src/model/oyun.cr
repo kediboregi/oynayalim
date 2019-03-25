7d8337cdda0e85c982a867a16a5ffd70fdf939d1
Oğuzhan Çankaya <unequaled86@gmail.com>
2019-03-25 17:23:20 +0300

    d

--- src/model/oyun.cr
+++ src/model/oyun.cr
@@ -2,6 +2,18 @@ class Oyun < Crecto::Model
     schema "oyunlar" do
         field :ad, String
 		field :bitti, Bool
+        field :uuid, String
+		has_many :eller, El
+    end
+end
+
+class El < Crecto::Model
+    schema "oyunlar" do
+        field :skor1, String
+		field :skor2, String
+		field :skor3, String
+		field :skor4, String
+		belongs_to :oyun, Oyun
     end
 end
 
@@ -48,6 +60,17 @@ class OyunApiRes
 
 	def parse
 		res = {"ad" => @ad, "bitti" => @bitti}
+		res.to_json
+	end
+end
+
+class ElApiRes
+	def initialize(skor1 : String, skor2 : String, skor3 : String, skor4 : String)
+
+	end
+
+	def parse
+		res = {"skor1" => @skor1, "skor2" => @skor2, "skor3" => @skor3, "skor4" => @skor4}
 		res.to_json
 	end
 end
--- src/oynayalim.cr
+++ src/oynayalim.cr
@@ -20,9 +20,14 @@ before_all do |env|
 	id = env.request.cookies["uuid"]?.try &.value
 	if id.nil?
 		#id = Random::Secure.hex
-		cook = HTTP::Cookie.new(name: "uuid", value: UUID.random.to_s, expires: Time.now + 24.years, secure: true)
+		id = UUID.random.to_s
+		cook = HTTP::Cookie.new(name: "uuid", value: id, expires: Time.now + 24.years, secure: true)
 		env.request.cookies << cook
 		env.response.cookies << cook
+	end
+
+	if id
+		env.set "uuid" id
 	end
 end
 
@@ -31,12 +36,14 @@ get "/" do |env|
   	{"uuid" => id}.to_json
 end
 
-get "/oyun" do |env|
+get "/oyun/:ad" do |env|
+	ad = env.params.url["ad"].as(String)
+
 	#query = Query.new
 	#query = query.where(ad: "cCc").limit(1)
 	#queryres = Repo.all(Oyun, query)
 
-	oyun = MyRepo.get_by(Oyun, ad: "cCc")
+	oyun = MyRepo.get_by(Oyun, ad: ad, uuid: env.get("uuid"))
 
 	if oyun
 		res = OyunApiRes.new oyun.ad.not_nil!, oyun.bitti.not_nil!
@@ -49,9 +56,13 @@ get "/oyun" do |env|
 end
 
 post "/oyun" do |env|
+	ad = env.params.json["ad"].as(String)
+    #bitti = env.params.json["bitti"].as(Bool)
+
 	oyun = Oyun.new
-	oyun.ad = "cCc"
+	oyun.ad = ad
 	oyun.bitti = false
+	oyun.uuid = env.get "uuid"
 	changeset = MyRepo.insert(oyun)
 	changeset.errors.any?
 	changeset.valid?
@@ -64,6 +75,40 @@ post "/oyun" do |env|
 		res.addeksik("cCc", "lol")
 		res.parse
 	end
+end
+
+post "/oyun/skor" do |env|
+	ad = env.params.json["ad"].as(String)
+	skor1 = env.params.json["skor1"].as(String)
+	skor2 = env.params.json["skor2"].as(String)
+	skor3 = env.params.json["skor3"].as(String)
+	skor4 = env.params.json["skor4"].as(String)
+
+	oyun = MyRepo.get_by(Oyun, ad: ad, uuid: env.get("uuid"))
+
+	el = El.new
+	el.skor1 = skor1
+	el.skor2 = skor2
+	el.skor3 = skor3
+	el.skor4 = skor4
+	el.oyun = oyun
+
+	changeset = MyRepo.insert(el)
+	changeset.errors.any?
+	changeset.valid?
+
+	if el
+		res = ElApiRes.new el.skor1.not_nil!, el.skor2.not_nil!, el.skor3.not_nil!, el.skor4.not_nil!
+		res.parse
+	else
+		res = EksikRes.new
+		res.addeksik("cCc", "lol")
+		res.parse
+	end
+end
+
+put "/oyun" do |env|
+
 end
 
 Kemal.run(ENV["PORT"].to_i32.not_nil!)
