a53a26f74100eed942c9e3d8fc81b8bbfc2a4084
Oğuzhan Çankaya <unequaled86@gmail.com>
2019-03-29 20:39:12 +0300

    fix

--- src/oynayalim.cr
+++ src/oynayalim.cr
@@ -30,7 +30,7 @@ end
 get "/oyun/:ad" do |env|
 	ad = env.params.url["ad"].as(String)
 	uuid = env.get("uuid")
-	oyun = Oyun.where { _ad == ad & (_user_uuid == uuid) }.includes(:eller).first!
+	oyun = Oyun.where { and(_ad == ad, _user_uuid == uuid) }.includes(:eller).first!
 
 	if oyun
 		eller = El.where { _oyun_id == oyun.id }.first
