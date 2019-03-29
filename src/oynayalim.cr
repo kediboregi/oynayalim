e5065cbea98c3973b8fd1d5acd39066699659be7
Oğuzhan Çankaya <unequaled86@gmail.com>
2019-03-29 19:08:24 +0300

    fix

--- src/oynayalim.cr
+++ src/oynayalim.cr
@@ -33,6 +33,7 @@ get "/oyun/:ad" do |env|
 	oyun = Oyun.where { _ad == ad & (_user_uuid == env.get("uuid")) }
 
 	if oyun
+		oyun.to_a
 		res = OyunApiRes.new oyun.ad.not_nil!, oyun.bitti.not_nil!
 		res.parse
 	else
@@ -69,9 +70,11 @@ post "/oyun/skor" do |env|
 	oyun = Oyun.where { _ad == ad & (_user_uuid == env.get("uuid")) }
 
 	if oyun
+		oyun.to_a
 		el = Oyun.build({:skor1 => skor1, :skor2 => skor2, :skor3 => skor3, :skor4 => skor4, :oyun_id => oyun.id})
 
 		if el.save
+			el.to_a
 			res = ElApiRes.new el.skor1.not_nil!, el.skor2.not_nil!, el.skor3.not_nil!, el.skor4.not_nil!
 			res.parse
 		else
